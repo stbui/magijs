8904db1fa6efed268c98233ac54076f36092fb3d
stbui <stbui@stbui.com>
2022-08-19 11:02:27 +0800

    feat: wip

--- packages/plugin-constraint/src/rules/DEP_LIMITED_RESOLUTIONS.ts
+++ packages/plugin-constraint/src/rules/DEP_LIMITED_RESOLUTIONS.ts
@@ -1,27 +1,28 @@
 import { IApi } from 'umi';
 
-const INVALID_PKGS = ['antd', 'antd-mobile', 'dva', 'qiankun', 'umi', 'umi-request'];
+const INVALID_PKGS = ['umi', 'umi-request', 'dva', 'qiankun'];
 
-export function validate(resolutions: any) {
-    const keys = Object.keys(resolutions);
-    for (const key of keys) {
-        if (INVALID_PKGS.includes(key)) return false;
-        if (key.startsWith(`@umijs/`)) return false;
-        if (key.startsWith(`@ant-design/`)) return false;
-        if (key.startsWith(`@magi/`)) return false;
-        if (key.startsWith(`@za/`)) return false;
-    }
-    return true;
+export function validate(dependencies: any) {
+  const keys = Object.keys(dependencies);
+  for (const key of keys) {
+    if (INVALID_PKGS.includes(key)) return false;
+    // if (key.startsWith(`@umijs/`)) return false;
+    // if (key.startsWith(`@ant-design/`)) return false;
+    // if (key.startsWith(`@magi/`)) return false;
+    // if (key.startsWith(`@za/`)) return false;
+  }
+  return true;
 }
 
 export default (api: IApi, opts: any) => {
-    api.onPluginReady(async () => {
-        const resolutions = api.pkg?.resolutions;
-        if (resolutions && !validate(resolutions)) {
-            opts.printError({
-                exit: true,
-                why: `你在 package.json 里却声明了 "resolutions": "${opts.highlight(JSON.stringify(resolutions))} 。"`,
-            });
-        }
-    });
+  api.onPluginReady(async () => {
+    const { dependencies = {}, devDependencies = {} } = api.pkg || {};
+    const resolutions = api.pkg?.resolutions;
+    if (!validate(dependencies) || !validate(devDependencies)) {
+      opts.printError({
+        exit: true,
+        why: `请删除在 package.json 里的声明： "${opts.highlight(JSON.stringify(INVALID_PKGS))} 。"`,
+      });
+    }
+  });
 };
