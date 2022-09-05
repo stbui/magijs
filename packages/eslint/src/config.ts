47ffb1997288bf005ccecb0437572d99599f5098
stbui <stbui@stbui.com>
2022-09-05 17:52:38 +0800

    fix: eslint ts不通过

--- packages/eslint/src/config.ts
+++ packages/eslint/src/config.ts
@@ -1,13 +1,17 @@
 module.exports = {
   root: true,
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    ecmaVersion: 8,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  extends: [require.resolve('eslint-config-za'), require.resolve('eslint-config-za/react')],
+  // parser: '@babel/eslint-parser',
+  // parserOptions: {
+  //   ecmaVersion: 8,
+  //   sourceType: 'module',
+  //   ecmaFeatures: {
+  //     jsx: true,
+  //   },
+  // },
+  extends: [
+    require.resolve('eslint-config-za'),
+    require.resolve('eslint-config-za/react'),
+    require.resolve('eslint-config-za/typescript'),
+  ],
   ignorePatterns: ['**/*.d.ts'],
 };
