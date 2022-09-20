413195f57c947f549273b3733af42b2a27aa34f8
stbui <stbui@stbui.com>
2022-09-20 17:59:11 +0800

    feat: 调整插件

--- packages/cli/src/preset.ts
+++ packages/cli/src/preset.ts
@@ -13,17 +13,20 @@ export default (api: IApi) => {
     require.resolve('@umijs/plugins/dist/analytics'),
     require.resolve('@umijs/plugins/dist/dva'),
     require.resolve('@umijs/plugins/dist/initial-state'),
-    require.resolve('@umijs/plugins/dist/layout'),
     require.resolve('@umijs/plugins/dist/model'),
     require.resolve('@umijs/plugins/dist/moment2dayjs'),
     require.resolve('@umijs/plugins/dist/request'),
     require.resolve('@umijs/plugins/dist/locale'),
-    require.resolve('@umijs/plugins/dist/qiankun'),
     require.resolve('@umijs/plugins/dist/tailwindcss'),
   ];
 
   if (api.userConfig.antd) {
-    plugins.push(require.resolve('@alita/plugins/dist/antd'));
+    plugins.push(require.resolve('@umijs/plugins/dist/antd'));
+    plugins.push(require.resolve('@umijs/plugins/dist/layout'));
+  }
+
+  if (api.userConfig.qiankun) {
+    plugins.push(require.resolve('@umijs/plugins/dist/qiankun'));
   }
 
   //
