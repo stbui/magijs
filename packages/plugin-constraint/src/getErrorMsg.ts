84f5b3c96b52d91cc569f3727f0573db91a7ddb1
stbui <stbui@stbui.com>
2023-09-12 11:40:34 +0800

    feat(constraint): 修改文档地址

--- packages/plugin-constraint/src/getErrorMsg.ts
+++ packages/plugin-constraint/src/getErrorMsg.ts
@@ -5,7 +5,7 @@ import chalk from '@magijs/compiled/chalk';
 const { red, green, white, bold } = chalk;
 
 export function getErrorMsg(opts: { ruleId: string; rule }, args: { why?: string; path?: any; node?: any } = {}) {
-    const ruleLink = `http://docs.test.za.biz/advanced/constraint#${opts.ruleId}`;
+    const ruleLink = `https://magi.zhonganonline.com/advanced/constraint#${opts.ruleId}`;
 
     const msg: any = [];
     if (args.path && !args.node) {
