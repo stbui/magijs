514a125de3d93187bc1bc74a07919d82fa89945b
stbui <stbui@stbui.com>
2023-10-11 11:20:56 +0800

    feat(lint): git hook

--- packages/lint/src/githooks.ts
+++ packages/lint/src/githooks.ts
@@ -4,19 +4,23 @@ import path from 'path';
 export const findHooksDir = () => {
   const dir = __dirname;
   const positon = dir.indexOf('node_modules');
-  const ROOT = dir.substring(0, positon);
 
-  let gitDir = path.join(ROOT, '.git');
-  if (!fs.existsSync(gitDir)) {
-    return '';
+  if (positon > -1) {
+    const ROOT = dir.substring(0, positon);
+    let gitDir = path.join(ROOT, '.git');
+    if (!fs.existsSync(gitDir)) {
+      return '';
+    }
+
+    return path.resolve(ROOT, gitDir, 'hooks');
   }
 
-  return path.resolve(ROOT, gitDir, 'hooks');
+  return '';
 };
 
 export const getCodeFilePath = filename => {
   if (filename) {
-    return path.join(__dirname, `../../githooks/${filename}`);
+    return path.join(__dirname, `../githooks/${filename}`);
   }
 
   return '';
@@ -25,17 +29,18 @@ export const getCodeFilePath = filename => {
 export const PreCommit = (dirHooks: string) => {
   const destPath = path.join(dirHooks, 'pre-commit');
   const srcPath = getCodeFilePath('pre-commit');
-  fs.copyFileSync(srcPath, destPath);
-
   console.log('[zalint]', 'install pre-commit', srcPath, destPath);
+
+  fs.copyFileSync(srcPath, destPath);
 };
 
 export const CommitMsg = (dirHooks: string) => {
   const destPath = path.join(dirHooks, 'commit-msg');
   const srcPath = getCodeFilePath('commit-msg');
-  fs.copyFileSync(srcPath, destPath);
 
   console.log('[zalint]', 'install commit-msg', srcPath, destPath);
+
+  fs.copyFileSync(srcPath, destPath);
 };
 
 const dirHooks = findHooksDir();
