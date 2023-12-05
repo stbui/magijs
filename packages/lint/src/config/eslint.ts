6cd4d57c1a353eb42dedd6ad9d349a0e21557f6f
stbui <stbui@stbui.com>
2023-12-05 18:58:51 +0800

    chore: wip

--- packages/lint/src/config/eslint.ts
+++ packages/lint/src/config/eslint.ts
@@ -7,13 +7,6 @@ module.exports = {
     require.resolve('eslint-config-prettier'),
   ],
 
-  rules: {
-    indent: 'error',
-    /** 用小驼峰命名法来命名你的对象、函数、实例 */
-    camelcase: 'error',
-    /** 用大驼峰命名法来命名类 */
-    'new-cap': 'error',
-  },
   parserOptions: {
     babelOptions: {
       plugins: [['@babel/plugin-proposal-decorators', { version: 'legacy' }]],
